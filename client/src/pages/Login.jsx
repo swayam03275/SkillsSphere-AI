3011ab259da66abb952466c6c5e0eb8e566a7fbf
BISWAJIT DAS <your.email@example.com>
2026-04-15 19:40:07 +0530

    feat: add client auth skeleton pages and routing

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div>Login Page (TODO)</div>;
+};
+
+export default Login;
---
+++ client/src/router/index.jsx
@@ -0,0 +1,15 @@
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+const routes = [
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+];
+
+export default routes;
