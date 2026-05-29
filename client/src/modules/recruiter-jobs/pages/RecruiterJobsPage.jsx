7231ca14a87db543af78bc293260b3cb7e92b5b6
Sahitya Chaddha <sahityachaddha.cs@gmail.com>
2026-05-29 19:35:07 +0530

    Fix Server and Client Tests: remove duplicate import and fix ReferenceError

--- client/src/modules/recruiter-jobs/pages/RecruiterJobsPage.jsx
+++ client/src/modules/recruiter-jobs/pages/RecruiterJobsPage.jsx
@@ -11,7 +11,6 @@ import EmptyState from "../../../shared/components/EmptyState";
 import JobCardSkeleton from "../../student-jobs/components/JobCardSkeleton";
 import { Pagination } from "../../../shared/components";
 import JobPostingCard from "../components/JobPostingCard";
-import { JobViewerCard, Pagination } from "../../../shared/components";
 import { useDocumentTitle } from "../../../hooks/useDocumentTitle";
 import {
 
