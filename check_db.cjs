faaf58a9e0085970a821992eaa559681c7e97313
Rishi Bhardwaj <rishibhardwaj1210@gmail.com>
2026-06-21 16:11:24 +0530

    docs: fix casing in check_db.cjs output

--- check_db.cjs
+++ check_db.cjs
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "server/.env" });
 
 mongoose.connect(process.env.MONGO_URI).then(async () => {
   const users = await mongoose.connection.collection("users").find({ email: /testauth/i }).toArray();
-  console.log("Found users:");
+  console.log("Found Users:");
   // Avoid printing password presence or any sensitive fields.
   users.forEach(u => console.log(u.email, u.name));
   process.exit(0);
